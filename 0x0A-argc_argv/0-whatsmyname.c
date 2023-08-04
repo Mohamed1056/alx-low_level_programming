235500fba617ae2e479ba49849e115d2fda31ca4
Mo_Gamal <mg8033667@mail.com>
2023-08-04 16:12:52 -0700

    shs

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ *
+ * @argc: takes the input
+ *
+ * @argv: takes the other input
+ *
+ * Return: 0 (success)
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
