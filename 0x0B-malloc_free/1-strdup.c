231c85d6593be0426c2df469932144f03b229bb5
Mo_Gamal <mg8033667@mail.com>
2023-08-08 11:14:06 -0700

    hgtfre

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include"main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_strdup - function to double any string
+ *
+ * @str: takes the input of the function
+ *
+ * Return: ptr if true null if false
+*/
+
+char *_strdup(char *str)
+{
+	int i, x;
+	/*after declaration*/
+	x = strlen(str);
+	char *ptr;
+	/*after declaration*/
+	ptr = malloc(x * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i <= x; ++i)
+		{
+			*(ptr + i) = *(str + i);
+		}
+	}
+	return (ptr);
+	free(ptr);
+}
