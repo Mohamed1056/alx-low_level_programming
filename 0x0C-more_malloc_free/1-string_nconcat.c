354d6ddf9d3632e7fa3e005ef94b7366bbf539f9
Mo_Gamal <mg8033667@mail.com>
2023-08-10 14:45:32 -0700

    adse

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,6 @@
  * second string
  * Return: str
 */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
@@ -20,9 +19,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 	for (len1 = 0; s1[len1] != '\0'; len1++)
-	;
+		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
-	;
+		;
 	str = malloc(len1 + n + 1);
 	if (str == NULL)
 	{
