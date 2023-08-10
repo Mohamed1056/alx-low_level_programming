973e09e14c176b960348257d5e9a1809fbdddb37
Mo_Gamal <mg8033667@mail.com>
2023-08-10 15:07:23 -0700

    adse

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,34 +12,37 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	int i, j, len1, len2, y = 0;
+	int i = 0, j = 0, k = 0, l = 0;
 	/*after decleration*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
-	if (n < len2)
-		y = n + 1;
-	else
-		y = len2 + 1;
-	str = malloc(sizeof(char) * (len1 + y));
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < len1; i++)
-	{
-		str[i] = s1[i];
-	}
-	for (j = 0; j < n; j++)
-	{
-		str[i] = s2[j];
+	while (s1[i])
 		i++;
+	while (s2[k])
+		k++;
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+		{
+			str[j] = s1[j];
+		}
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
 	}
-	str[i] = '\0';
+	str[j] = '\0';
 	return (str);
 }
