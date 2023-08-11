222d68d35eaad6fd92793c351a09fd9e35226447
Mo_Gamal <mg8033667@mail.com>
2023-08-11 12:35:27 -0700

    afrwd

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,14 +30,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	ptur = ptr;
 	if (new_size < old_size)
 	{
-		for (i = 0; i < old_size; i++)
+		for (i = 0; i < new_size; i++)
 		{
 			pttr[i] = ptur[i];
 		}
 	}
 	if (new_size > old_size)
 	{
-		for (i = 0; i < new_size; i++)
+		for (i = 0; i < old_size; i++)
 		{
 			pttr[i] = ptur[i];
 		}
