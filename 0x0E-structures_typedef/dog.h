99d68a2255980bcfff82a9562cfd7fcccbf11071
Mo_Gamal <mg8033667@mail.com>
2023-08-14 13:40:02 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: structure for dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
