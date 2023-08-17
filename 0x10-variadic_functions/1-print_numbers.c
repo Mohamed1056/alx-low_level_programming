1f53dd7f31b330c6b35e3e92c40a4ce558f8e6da
Mo_Gamal <mg8033667@mail.com>
2023-08-17 14:32:56 -0700

    hhsus

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include"variadic_functions.h"
+
+/**
+ * print_numbers - function to print nums
+ * @separator: takes the input of the function
+ * @n: takes the num of argument
+ * Return: nothing
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	int s;
+	va_list ptr;
+
+	va_start(ptr, n);
+	while (i--)
+	{
+		s = va_arg(ptr, int);
+		printf("%d", s);
+		if (separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr);
+}
