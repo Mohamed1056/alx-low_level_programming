2c0cd73abc8eabeef46a9696d84bd61d1374f6c4
Mo Gamal <mg8033667@gmail.com>
2023-08-24 16:42:12 -0700

    commiting again and again

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,32 +2,17 @@
 #include <stdio.h>
 
 /**
- * _strlen - function to find the length
+ * print_list - function to find the length
  * of a string
- * @str: takes the input for the function
- * Return: i
-*/
-int _strlen(char *str)
-{
-	int i;
-
-	if (!str)
-		return (0);
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
- * print_list - function to print a linked list
- * @h : takes the input of the function
+ * @h: takes the input for the function
  * Return: k
 */
+
 
 size_t print_list(const list_t *h)
 {
 	size_t k = 0;
-
+	/*after decleration*/
 	while (h)
 	{
 		if (!h->str)
