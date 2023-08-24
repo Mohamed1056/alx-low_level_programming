df369a394256bee8860176399846d9ab2ad72087
Mo Gamal <mg8033667@gmail.com>
2023-08-24 16:36:44 -0700

    commiting again

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -33,7 +33,7 @@ size_t print_list(const list_t *h)
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%lu] %s\n", _strlen(h->str), h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		k++;
 	}
