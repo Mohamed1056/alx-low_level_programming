c2a725bbcee0a0a39f2d50b4caa0b1e6601a9a84
Mo Gamal <mg8033667@gmail.com>
2023-08-24 16:54:46 -0700

    commiting again and again

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,32 +4,34 @@
  * add_node_end - function
  * @head: takes 1st input
  * @str: takes 2nd input
- * Return: new_noode
+ * Return: nexw
 */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_noode = malloc(sizeof(list_t));
-	list_t *node = *head;
+	list_t *nexw;
+	list_t *tump = *head;
+	unsigned int len = 0;
+	/*after decleration*/
+	while (str[len])
+		len++;
 
-	if (!head || !new_noode)
+	nexw = malloc(sizeof(list_t));
+	if (!nexw)
 		return (NULL);
-	if (str)
+
+	nexw->str = strdup(str);
+	nexw->len = len;
+	nexw->next = NULL;
+	/*if statment to check the code*/
+	if (*head == NULL)
 	{
-		new_noode->str = strdup(str);
-		if (!new_noode->str)
-		{
-			free(new_noode);
-			return (NULL);
-		}
+		*head = nexw;
+		return (nexw);
 	}
-		if (node)
-		{
-			while (node->next)
-				node = node->next;
-			node->next = new_noode;
-		}
-		else
-			*head = new_noode;
-		return (new_noode);
+	while (tump->next)
+		tump = tump->next;
+
+	tump->next = nexw;
+	return (nexw);
 }
+
