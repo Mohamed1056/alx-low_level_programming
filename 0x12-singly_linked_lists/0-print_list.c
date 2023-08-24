d51422c373d84771deea4f5cc1a4d751bca39122
Mo Gamal <mg8033667@gmail.com>
2023-08-24 14:30:47 -0700

    1st commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include"lists.h"
+
+/**
+ * _strlen - function to find the length
+ * of a string
+ * @str: takes the input for the function
+ * Return: i
+*/
+int _strlen(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - function to print a linked list
+ * @h : takes the input of the function
+ * Return: m
+*/
+size_t print_list(const list_t *h)
+{
+	size_t m = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		m++;
+	}
+	return (m);
+}
