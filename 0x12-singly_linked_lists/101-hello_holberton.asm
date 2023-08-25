96347a20ef17e99296a83483c77e610bade2fa74
Mo Gamal <mg8033667@gmail.com>
2023-08-24 17:18:04 -0700

    commiting again and again

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,10 +2,14 @@ section		.text
 	extern	printf
 	global	main
 
+/*before main*/
+
 main:
 	mov		edi, msg
 	mov		eax, 0
 	call	printf
+
+/*after main*/
 
 swction		.data
 	msg db 'Hello, Holberton', 0xa, 0
