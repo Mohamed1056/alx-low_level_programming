2da0417329bc8a22249b246f11ae41a1b38299cf
Mo Gamal <mg8033667@gmail.com>
2023-08-28 12:45:15 -0700

    6th commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,19 +8,10 @@
 
 void free_listint2(listint_t **head)
 {
-	if (head == NULL)
+	while (*head != NULL)
 	{
-		return (NULL);
-	}
-	else
-	{
-		listint_t *temp;
-
-		while (head != NULL)
-		{
-			temp = head;
-			head = head->next;
-		}
-		head->next = NULL;
+		listint_t changer = *head;
+		*head = (*head)->next;
+		free(changer);
 	}
 }
