ec70f5cc0fddcdc61eb70e9824f7c91e39e5aabd
Mo Gamal <mg8033667@gmail.com>
2023-08-28 16:59:40 -0700

    8th commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i;
 	listint_t *search;
 
-	for (search = head, i = 0; search && i < index; search = serach->next, i++)
+	for (search = head, i = 0; search && i < index; search = search->next, i++)
 		;
 	return (search);
 }
