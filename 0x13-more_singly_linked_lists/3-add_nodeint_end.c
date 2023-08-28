a1ae8b46c5edb0bccd87efbac900b70e21408425
Mo Gamal <mg8033667@gmail.com>
2023-08-28 11:34:34 -0700

    4th commit

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include"lists.h"
+
+/**
+ * add_nodeint_end - add node at the end
+ * @head: takes the input for the function
+ * @n: take the 2nd input
+ * Return: temp
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp;
+
+	temp = (listint_t *)malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->n = n;
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = *temp;
+	}
+	else
+	{
+		listint_t *current = *head;
+
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = temp;
+	}
+	return (temp);
+}
