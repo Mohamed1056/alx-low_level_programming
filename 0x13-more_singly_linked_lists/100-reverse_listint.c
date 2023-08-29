0bb3f11b7853b70cb660e98e85e5f2bc2b2c1a09
Mo Gamal <mg8033667@gmail.com>
2023-08-29 04:55:40 -0700

    12th commit

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include"lists.h"
+
+/**
+ * reverse_listint - fuunction to reverse the list
+ * @head: takes the input for the function
+ * Return: head
+*/
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_int *current = *head;
+
+	while (current != NULL)
+	{
+		listint_t *next_node = current->next;
+
+		current->next = prev;
+		prev = current;
+		current = next_node;
+	}
+	*head = prev;
+	return (*head);
+}
