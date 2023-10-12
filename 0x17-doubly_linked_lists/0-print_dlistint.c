f9cead9ec63d2a60817d34276152f1d6e506fbf9
Mohamed Gamal <mohamed@gmail.com>
2023-10-12 15:09:48 -0700

    adding the first file

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function to print the
+ * elements in a list
+ * @h: the head of the list
+ * Return: number of element
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int num = 0;
+	/*after decleration*/
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
