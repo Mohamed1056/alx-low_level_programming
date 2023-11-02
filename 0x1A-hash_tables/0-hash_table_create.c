052a42620eafb846f52f29bb44d97c9fb5fd0127
Mohamed Gamal <mohamed@gmail.com>
2023-11-02 14:28:22 -0700

    adding readme file and the first file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned long int i;
 	/*after decleration of the variables*/
+	if (size == 0)
+		return (NULL);
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
@@ -18,7 +20,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 	{
-		free(hash_table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
